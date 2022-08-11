50a21928c092b96f288fde986b7e36dcf885c831
Gena <gena.hurst@yahoo.com>
2022-08-11 12:14:56 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 import { createComment } from "./comcomp";
 import "./style.css"
 
-document.querySelector('#app').innerHTML = `
-<div>
-<header>
-<h1>Comment App</h1>
+document.querySelector('#app').innerHTML = 
+
+`<div>
+<header id="header" class="header">
+<h1>The Comment App</h1>
 </header>
 <div id="result-grid" class = "result-grid">
         <form id="form">
@@ -13,7 +14,7 @@ document.querySelector('#app').innerHTML = `
             <input type="text" id="name" class="name" placeholder="Name" required><br>
             <label for="email"><h3><span>Email: </span></label>
             <input type="email" id="email" class="email" placeholder="Email" required>
-            <label for="comment"><h3>Comment:</h3></label>
+            <label for="comment"><h3>Comment on Photo:</h3></label>
             <textarea type="text" id="comment" class="comment" cols="50" rows="4" required>
             </textarea><br><br>
             <input type="checkbox" id="checkbox" name="checkbox" checked required>
@@ -21,12 +22,19 @@ document.querySelector('#app').innerHTML = `
             <input value="Post My Comment" type="submit" id="submit"><br>
             </div>
             </form> 
-            </div>
             
-            <div id=comcomp>
-             
+            <div id=photoholder class="photoholder" >
+            </div>
+            <div id="comcomp" class="comcomp">
+            <h6>Posted Comments</>
       </div>
+            </div>
+      
       </div>
+
+    
+
+ 
 <template id="template">
 <h1 id="nametemp"></h1>
 <h3 id="emailtemp"></h3>
